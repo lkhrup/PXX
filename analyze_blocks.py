ae55e486e0411ee1c6de072ee908f6c74692826f
Lock Her Up <68363677+lkhrup@users.noreply.github.com>
2024-08-16 18:28:24 +0200

    improve analysis (hopefully)

--- analyze_blocks.py
+++ analyze_blocks.py
@@ -23,10 +23,11 @@ prompt_prefix = """
 Instruction:
 From the SEC filing input, extract the vote cast on TESLA, INC (ticker TSLA) issue/proposal number 1 on the 21-Mar-18 special meeting.
 Issue 1 is titled "Approve Grant of Performance-Based Stock Option Award" but another phrasing such as "Approve Stock Option Grant to Elon Musk" or "Approval of Performance Stock Option Agreement" may be used.
-Consider only the issue mentioned, disregard any other issues/proposals in the input.
-Consider only the actual vote, ignore any Management Recommendation.
-Your response must be a single word, do not explain your reasoning.
-Your response must be "For" or "Against", or "None" if no vote was cast on the specific proposal mentioned, "Multiple" if the input contains multiple instances of the same meeting dates and proposals, or "Absent" if the input is not relevant.
+Consider only the issue mentioned, disregard any other issues in the input.
+Consider only the actual vote, ignore the Management Recommendation (which is For in this case). If you see "Mgt Rec Vote Cast" in the input, then pick the second vote mentioned (so "For Against" would be Against, not For). The text is preformatted, so keeping track of column alignments may help.
+Your response imperatively must always be a single word, do not explain your reasoning.
+Your response must be "None" if the input does not include a vote on the specific issue mentioned.
+Otherwise, it must be either "For" or "Against".
 
 Input:
 """.strip()
@@ -36,7 +37,6 @@ prompt_suffix = """
 client = Client(host=os.environ.get('OLLAMA_HOST', 'http://127.0.0.1:11434'))
 
 def analyze_blocks(row):
-    print(row['url'])
     url = row['url']
     cik = row['cik']
     filename = row['filename']
@@ -44,6 +44,7 @@ def analyze_blocks(row):
     filename = filename.replace('.txt', '.json')
     with open(f'blocks/{filename}') as f:
         blocks = json.loads(f.read())
+    print(filename)
     for block in blocks:
         fund = block['fund']
         # If fund is an array (not a string), it is an array (fund, ticker_symbol)
@@ -63,12 +64,15 @@ def analyze_blocks(row):
         vote = response['message']['content']
         print(f"{fund}: {vote}")
         vote = vote.lower().strip()
+        if len(vote) > 7:
+            vote = 'confused'
         # Validate and store vote
         if vote in ['for', 'against', 'none']:
             conn.execute("""
                 INSERT OR REPLACE INTO votes (key, url, fund, ticker_symbol, fund_line, block_text, vote)
                 VALUES (?, ?, ?, ?, ?, ?, ?)
             """, (f"{cik} {fund}", url, fund, ticker_symbol, fund_line, block_text, vote))
+            conn.commit()
 
 
 # For every filing in sqlite:
