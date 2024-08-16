ef40c40679c58eef75dc81752b362589f8054a75
Lock Her Up <68363677+lkhrup@users.noreply.github.com>
2024-08-16 16:34:58 +0200

    update block analysis

--- analyze_blocks.py
+++ analyze_blocks.py
@@ -1,10 +1,23 @@
 import os
+import json
 from ollama import Client
 import pysqlite3
 
 year = 2018
 conn = pysqlite3.connect(os.environ.get('SQLITE_PATH', f'{year}.sqlite'))
 conn.row_factory = pysqlite3.Row
+conn.execute("""
+CREATE TABLE IF NOT EXISTS votes (
+    key TEXT PRIMARY KEY,
+    url TEXT,
+    fund TEXT,
+    ticker_symbol TEXT,
+    fund_line TEXT,
+    block_text TEXT,
+    vote TEXT
+);
+""")
+conn.commit()
 
 prompt_prefix = """
 Instruction:
@@ -22,26 +35,45 @@ prompt_suffix = """
 
 client = Client(host=os.environ.get('OLLAMA_HOST', 'http://127.0.0.1:11434'))
 
-# For every filing in sqlite:
-for row in conn.execute('SELECT * FROM filings WHERE prop1 is NULL'):
-    print(f"{row['prop1']}: {row['url']}")
-    if row['prop1'] is not None:
-        break
+def analyze_blocks(row):
+    print(row['url'])
     url = row['url']
-    filename = row['filename'].replace('.htm', '.txt')
+    cik = row['cik']
+    filename = row['filename']
+    filename = filename.replace('.htm', '.txt')
+    filename = filename.replace('.txt', '.json')
     with open(f'blocks/{filename}') as f:
-        content = f.read()
-    content = prompt_prefix + "\n" + content + "\n" + prompt_suffix
-    response = client.chat(model='llama3:70b', messages=[
-        {
-            'role': 'user',
-            'content': content,
-        },
-    ])
-    vote = response['message']['content']
-    print(f"{filename}: {vote}")
-    vote = vote.lower().strip()
-    # Validate and store vote
-    if vote in ['for', 'against', 'none']:
-        conn.execute('UPDATE filings SET prop1 = ? WHERE url = ?', (vote, url))
-        conn.commit()
+        blocks = json.loads(f.read())
+    for block in blocks:
+        fund = block['fund']
+        # If fund is an array (not a string), it is an array (fund, ticker_symbol)
+        ticker_symbol = None
+        if isinstance(fund, list):
+            fund, ticker_symbol = fund
+        fund_line = block['fund_line']
+        text_blocks = block['blocks']  # Array of array of lines
+        block_text = "\n".join(["\n".join(lines) for lines in text_blocks])
+        content = prompt_prefix + "\n" + block_text + "\n" + prompt_suffix
+        response = client.chat(model='llama3:70b', messages=[
+            {
+                'role': 'user',
+                'content': content,
+            },
+        ])
+        vote = response['message']['content']
+        print(f"{fund}: {vote}")
+        vote = vote.lower().strip()
+        # Validate and store vote
+        if vote in ['for', 'against', 'none']:
+            conn.execute("""
+                INSERT OR REPLACE INTO votes (key, url, fund, ticker_symbol, fund_line, block_text, vote)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (f"{cik} {fund}", url, fund, ticker_symbol, fund_line, block_text, vote))
+
+
+# For every filing in sqlite:
+for row in conn.execute('SELECT * FROM filings'):
+    try:
+        analyze_blocks(row)
+    except FileNotFoundError:
+        print(f"Warning: no blocks for {row['url']}")
