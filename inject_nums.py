69811f07a11d0498c6469bb9de477ca77ec2a4b3
Lock Her Up <68363677+lkhrup@users.noreply.github.com>
2024-08-19 10:01:48 +0200

    inject notes into database

--- inject_nums.py
+++ inject_nums.py
@@ -8,7 +8,7 @@ year = 2018
 conn = sqlite3.connect(os.environ.get('SQLITE_PATH', f'{year}.sqlite'))
 conn.row_factory = sqlite3.Row
 
-# Load the the cik -> num map.
+# Load the cik -> num map.
 with open(f'{year}-nums.json', 'r', encoding="utf-8") as f:
     num_dict = json.loads(f.read())
 
@@ -22,3 +22,14 @@ for row in conn.execute('SELECT * FROM filings'):
         print(f"Updated {cik} to {num}")
     else:
         print(f"Warning: no num for {cik}")
+
+# Look for a notes.json file
+try:
+    with open(f'{year}-notes.json', 'r', encoding="utf-8") as f:
+        notes_dict = json.loads(f.read())
+    for num, note in notes_dict.items():
+        conn.execute('UPDATE filings SET note = ? WHERE num = ?', (note, num))
+        conn.commit()
+        print(f"Updated note for {num}")
+except FileNotFoundError:
+    pass
